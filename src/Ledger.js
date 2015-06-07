0075137237dc42728f6c133d303529a2cc189305
Andy Berry <andyberry88@gmail.com>
2015-06-07 17:26:59 +0100

    adding support to process ledger to a given date

--- src/Ledger.js
+++ src/Ledger.js
@@ -1,16 +1,19 @@
 (function(window) {
 	"use strict";
 	
-	var Ledger = function (ledgerInput) {
+	var Ledger = function (ledgerInput, toDate) {
 		this._accounts = {};
 		if (typeof ledgerInput === "string") {
 			ledgerInput = [ledgerInput];
 		}
+		var processToDate = (toDate === undefined) ? new Date() : parseDate(toDate);
 		for (var i = 0; i < ledgerInput.length; i++) {
 			var processedLine = processLine(ledgerInput[i]);
-			var amount = processedLine.amount;
-			this.getAccount(processedLine.name).removeValue(amount);
-			this.getAccount(processedLine.destination).addValue(amount);
+			if (processedLine.date <= processToDate) {
+				var amount = processedLine.amount;
+				this.getAccount(processedLine.name).removeValue(amount);
+				this.getAccount(processedLine.destination).addValue(amount);
+			}
 		}
 	};
 
@@ -51,11 +54,18 @@
 	var processLine = function(line) {
 		var lineSplit = line.split(",");
 		return {
-			"date" : lineSplit[0],
+			"date" : parseDate(lineSplit[0]),
 			"name" : lineSplit[1],
 			"destination" : lineSplit[2],
 			"amount" : parseInt(lineSplit[3])
 		};
+	};
+	var parseDate = function(dateInput) {
+		var dateArray = /([0-9]{4})-([0-9]{2})-([0-9]{2})/.exec(dateInput);
+		var year = dateArray[1];
+		var month = dateArray[2];
+		var day = dateArray[3];
+		return new Date(year, month, day);
 	};
 
 
--- tests/LedgerTest.js
+++ tests/LedgerTest.js
@@ -2,6 +2,11 @@
 "use strict";
 
 	describe("Ledger Test", function() {
+
+		it("account amount starts at 0", function() {
+			var ledger = new Ledger([]);
+			expect(ledger.totalFor("john")).toBe(0);
+		});
 		
 		it("should process a ledger with a single line", function() {
 			var ledger = new Ledger("2015-01-16,john,mary,125.00");
@@ -40,6 +45,30 @@
 			expect(ledger.totalFor("supermarket")).toBe(20);
 			expect(ledger.totalFor("insurance")).toBe(100);
 			expect(ledger.totalFor("bob")).toBe(50);
+		});
+		
+		it("should process a ledger up to a given date", function() {
+			var ledger = new Ledger([
+				"2015-01-16,john,mary,125.00",
+				"2015-01-17,john,supermarket,20.00",
+				"2015-01-17,mary,insurance,100.00",
+				"2015-01-17,mary,bob,100.00",
+				"2015-01-17,bob,john,50.00"
+			], "2015-01-16");
+			expect(ledger.totalFor("john")).toBe(-125);
+			expect(ledger.totalFor("mary")).toBe(125);
+		});
+		
+		it("should process a ledger up to a given date if transactions arent in order", function() {
+			var ledger = new Ledger([
+				"2015-01-17,john,supermarket,20.00",
+				"2015-01-17,mary,insurance,100.00",
+				"2015-01-16,john,mary,125.00",
+				"2015-01-17,mary,bob,100.00",
+				"2015-01-17,bob,john,50.00"
+			], "2015-01-16");
+			expect(ledger.totalFor("john")).toBe(-125);
+			expect(ledger.totalFor("mary")).toBe(125);
 		});
 		
 	});
