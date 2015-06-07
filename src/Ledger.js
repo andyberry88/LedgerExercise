f5a8ed9153d140e36a87944fd356a5d0312cce62
Andy Berry <andyberry88@gmail.com>
2015-06-07 16:48:16 +0100

    processing a ledger with a single line

--- src/Ledger.js
+++ src/Ledger.js
@@ -1,5 +1,58 @@
-"use strict";
-
-var Ledger = function () {
+(function(window) {
+	"use strict";
 	
-}
+	var Ledger = function (ledgerInput) {
+		this._accounts = {};
+		var processedLine = processLine(ledgerInput);
+		var amount = processedLine.amount;
+		this.getAccount(processedLine.name).removeValue(amount);
+		this.getAccount(processedLine.destination).addValue(amount);
+	};
+
+	Ledger.prototype.getAccount = function(name) {
+		var account = this._accounts[name];
+		if (account === undefined) {
+			account = new Account();
+			this._accounts[name] = account;
+		}
+		return account;
+	};
+
+	Ledger.prototype.totalFor = function(name) {
+		return this.getAccount(name).amount();
+	};
+	
+	
+	
+	
+	var Account = function() {
+		this._amount = 0;
+	};
+	
+	Account.prototype.amount = function() {
+		return this._amount;
+	};
+	
+	Account.prototype.addValue = function(val) {
+		this._amount += val;
+	};
+	
+	Account.prototype.removeValue = function(val) {
+		this._amount -= val;
+	};
+
+
+
+	var processLine = function(line) {
+		var lineSplit = line.split(",");
+		return {
+			"date" : lineSplit[0],
+			"name" : lineSplit[1],
+			"destination" : lineSplit[2],
+			"amount" : parseInt(lineSplit[3])
+		};
+	};
+
+
+	window.Ledger = Ledger;
+})(window);
--- tests/LedgerTest.js
+++ tests/LedgerTest.js
@@ -1,5 +1,12 @@
+(function() {
 "use strict";
 
-describe("Ledger Test", function() {
-	
-});
+	describe("Ledger Test", function() {
+		it("should process a ledger with a single line", function() {
+			var ledger = new Ledger("2015-01-16,john,mary,125.00");
+			expect(ledger.totalFor("john")).toBe(-125);
+			expect(ledger.totalFor("mary")).toBe(125);
+		});
+	});
+
+})();
